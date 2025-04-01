71427cb0da9f62dd1c810d6a5728da2901b9bc12
Chika Kurihara <chikakurihara1993@gmail.com>
2025-04-01 15:28:34 -0400

    Add ConfirmPassword

--- LibrarySystemProject/Models/User.cs
+++ LibrarySystemProject/Models/User.cs
@@ -14,23 +14,29 @@ namespace LibrarySystemProject.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserID { get; set; } 
 
-        [Required]
+        [Required(ErrorMessage = "Please enter your first name")]
         [StringLength(100)]
         public string FirstName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter your last name")]
         [StringLength(100)]
         public string LastName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter your email address")]
         [StringLength(100)]
         [EmailAddress]
         public string Email { get; set; } 
 
-        [Required]
+        [Required(ErrorMessage = "Please enter your password")]
         [StringLength(255)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage ="Please confirm your password")]
+        [StringLength(255)]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
         public virtual ICollection<RentalTracking> Rentals { get; set; }
         public virtual ICollection<WishList> WishLists { get; set; }
     }
